ab1ae2cb943e26c2a025a269a80a3ebf0999dbe3
SergeyK1983 <ssp-serg@yandex.ru>
2023-12-21 18:28:59 +0300

    serializer1

--- Pereval/perevalinfo/serializer.py
+++ Pereval/perevalinfo/serializer.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
+from rest_framework.relations import PrimaryKeyRelatedField
+
 from .models import Users, PerevalAdded, Coords, DifficultyLevel, PerevalImages
 
 
 class UsersSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Users
         fields = (
@@ -13,7 +14,42 @@ class UsersSerializer(serializers.ModelSerializer):
         )
 
 
+class CoordsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coords
+        fields = (
+            'latitude',
+            'longitude',
+            'height',
+        )
+
+
+class DifficultyLevelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DifficultyLevel
+        fields = (
+            'winter',
+            'spring',
+            'summer',
+            'autumn',
+        )
+
+
+class PerevalImagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PerevalImages
+        fields = (
+            'images',
+        )
+
+
 class PerevalAddedSerializer(serializers.ModelSerializer):
+    pereval = UsersSerializer(label='Отправитель')
+    coord_id = CoordsSerializer(label='Координаты')
+    level_id = DifficultyLevelSerializer(label='Уровень сложности')
+    images1 = PerevalImagesSerializer(label='Фотография 1')
+    images2 = PerevalImagesSerializer(label='Фотография 2')
+    images3 = PerevalImagesSerializer(label='Фотография 3')
 
     class Meta:
         model = PerevalAdded
@@ -21,4 +57,12 @@ class PerevalAddedSerializer(serializers.ModelSerializer):
             'beautyTitle',
             'title',
             'other_titles',
+            'connect',
+            'pereval',  # по related_name='pereval'
+            'coord_id',
+            'level_id',
+            'images1',
+            'images2',
+            'images3',
         )
+
